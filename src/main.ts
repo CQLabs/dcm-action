58f9ed08028e609985d871fe5fd5db4c0ea5e674
Dmitry Zhifarsky <dimannich2380@gmail.com>
2023-07-10 11:38:22 +0400

    chore: add logs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import * as io from '@actions/io';
-import { analyze, getConclusion } from './commands/analyze';
+// import { analyze, getConclusion } from './commands/analyze';
 import { getOptions } from './options';
-import { Reporter } from './reporter/reporter';
+// import { Reporter } from './reporter/reporter';
 // import { setGitHubAuth } from './auth';
 
 async function run(): Promise<void> {
@@ -15,20 +15,53 @@ async function run(): Promise<void> {
 
     core.startGroup('Analyzing');
 
-    const reports = await analyze(options);
-    const conclusion = getConclusion(reports, options);
+    core.info(`Creating check run DCM`);
+    const octokit = github.getOctokit(options.token);
+    const createResp = await octokit.rest.checks.create({
+      head_sha: github.context.sha,
+      name: 'DCM',
+      status: 'in_progress',
+      output: {
+        title: 'DCM',
+        summary: '',
+      },
+      ...github.context.repo,
+    });
+
+    core.info('Creating report summary');
+    const summary = `Hello
+world
+`;
+
+    const resp = await octokit.rest.checks.update({
+      check_run_id: createResp.data.id,
+      conclusion: 'success',
+      status: 'completed',
+      output: {
+        title: `DCM`,
+        summary,
+      },
+      ...github.context.repo,
+    });
+
+    core.info(`Check run create response: ${resp.status}`);
+    core.info(`Check run URL: ${resp.data.url}`);
+    core.info(`Check run HTML: ${resp.data.html_url ?? ''}`);
+
+    // const reports = await analyze(options);
+    // const conclusion = getConclusion(reports, options);
     // get summary
 
-    const reporter = new Reporter(github.getOctokit(options.token));
-    const runner = await reporter.create(options.reportTitle);
-    await reporter.reportIssues(reports, runner.data.id, conclusion, options.reportTitle);
-    await reporter.postComment(`## Hello`);
+    // const reporter = new Reporter(github.getOctokit(options.token));
+    // const runner = await reporter.create(options.reportTitle);
+    // await reporter.reportIssues(reports, runner.data.id, conclusion, options.reportTitle);
+    // await reporter.postComment(`## Hello`);
 
     core.endGroup();
 
-    if (conclusion === 'failure') {
-      core.setFailed('Found fatal issues!');
-    }
+    // if (conclusion === 'failure') {
+    //   core.setFailed('Found fatal issues!');
+    // }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
